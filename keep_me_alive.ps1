73fcfb2bd537dc8753536e7e6880f281aeeba29a
gopiperu <gopi1043@gmail.com>
2020-06-07 09:45:23 -0400

    Create keep_me_alive.ps1
    Power shell script to keep the system alive.
    variable - interval is set to 5 minutes. This can be updated.
    $interval = 5

---
+++ keep_me_alive.ps1
@@ -0,0 +1,19 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+$interval = 5
+
+    while(1) {  
+    $position = [System.Windows.Forms.Cursor]::Position  
+    $position.X++  
+    [System.Windows.Forms.Cursor]::Position = $position
+    $position1 = [System.Windows.Forms.Cursor]::Position
+    $position1.X--
+    [System.Windows.Forms.Cursor]::Position = $position1    
+
+    $time = Get-Date;  
+    $shorterTimeString = $time.ToString("HH:mm:ss");  
+
+    Write-Host $shorterTimeString "Mouse pointer has been moved 1 pix"  
+    #Set your duration between each mouse move
+    Start-Sleep -Seconds ($interval * 60)
+    } 
